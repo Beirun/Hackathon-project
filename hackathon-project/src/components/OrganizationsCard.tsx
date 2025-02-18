26da842ebf173595c6c4867e008f13740990974d
Beirun <orillobernardjay@gmail.com>
2025-02-18 16:28:56 +0800

    commit

--- hackathon-project/src/components/OrganizationsCard.tsx
+++ hackathon-project/src/components/OrganizationsCard.tsx
@@ -6,13 +6,13 @@ const OrganizationsCard = () => {
         <div className="w-[20%] h-full">
             <div style={{ backgroundImage: `url(../src/assets/logo-1.png)`, backgroundSize: "cover", backgroundPosition: "center" }} className="w-full h-full rounded-md"/>
         </div>
-        <div className="w-[80%] h-full bg-green-100 flex flex-col">
+        <div className="w-[80%] h-full flex flex-col">
             <div className="w-full h-[55%]">
             <div style={{ backgroundImage: `url(../src/assets/projects-1.jpg)`, backgroundSize: "cover", backgroundPosition: "center" }} className="w-full h-full rounded-tr-md"/>
             </div>
             <div className="flex flex-col w-full h-[45%] px-5 gap-2">
-                <p className="text-2xl">Test</p>
-                <p className="text-sm">Test</p>
+                <p className="text-2xl">Rise Above Foundation Cebu, INC</p>
+                <p className="text-sm">A Non-Profit NGO, an active group of people who focuses on improving the quality of life for the underprivileged families in Cebu, Philippines. </p>
             </div>
         </div>
     </div>
